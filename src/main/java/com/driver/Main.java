953f5308a69de580ad563a1ca72283c6e6f25107
aashish kumar yadav <aashish8174@gmail.com>
2023-05-12 12:44:31 +0530

    override methode implemented

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main (String args[]){
+        B b = new B();
+        System.out.println(b.meth());
+    }
+  public static class A{
+      public String meth(){
+          return "Invoking method from class A";
+      }
+  }
+  B b  = new B();
+ String str = b.meth();
+  public static class B extends A{
+      @Override
+      public String meth() {
+          return "Methode is overridden in extended class B";
+      }
+     /// public String met(){
+        //  return super.meth();
+      //}
+  }
 }
